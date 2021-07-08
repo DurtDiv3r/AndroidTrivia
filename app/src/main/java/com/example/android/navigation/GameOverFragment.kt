ec6f610d6209134e5b57764d33970c84158145cb
DurtDiv3r <75084231+DurtDiv3r@users.noreply.github.com>
2021-07-08 10:45:25 +0200

    Complete

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -22,6 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.durtdev.example.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -30,6 +31,10 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+
+        binding.tryAgainButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(GameOverFragmentDirections.actionGameOverFragment2ToGameFragment2())
+        )
         return binding.root
     }
 }
--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,11 +1,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.durtdev.example.navigation.R
 import com.durtdev.example.navigation.databinding.FragmentTitleBinding
 
@@ -19,6 +20,33 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+//        binding.playButton.setOnClickListener {view: View ->
+////            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment2)
+//            //or
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment2)
+//        }
+        //or
+
+//        binding.playButton.setOnClickListener(
+//            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment2)
+//        )
+        //or
+
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment2())
+        }
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
